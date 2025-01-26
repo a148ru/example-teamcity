68778abea3af472f16086148488682a8a0927514
Anton <ant.band@mail.ru>
2025-01-27 01:42:13 +0300

    hunter 2

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-	public String sayHant(){
-		return " say hanter";
+	public String sayHunt(){
+		return "say hunter";
 	}
 }
